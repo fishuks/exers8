bf22a94913858d59f6d7d6546a2ca16bbbad24a2
fishukaa <103937450+fishuks@users.noreply.github.com>
2024-03-23 00:39:24 +0700

    fix task9

--- task9.py
+++ task9.py
@@ -3,9 +3,16 @@ import xmltodict
 import yaml
 
 def to_json(function):
-    def wrapped(*args, **kwargs):
-        result = function(*args, **kwargs)
-        json_parameter = json.dumps(result)
-        return json_parameter
+    def wrapped(format = 'json'):
+        result = function(format = 'json')
+        if format == 'json':
+            json_param = json.dumps(result)
+            return json_param
+        elif format == 'xml':
+            xml_param = xmltodict.unparse(result)
+            return xml_param
+        elif format == 'yaml':
+            yaml_param =  yaml.dump(result)
+            return  yaml_param
     return wrapped
 
